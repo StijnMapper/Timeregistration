2b5b8e2eeafdd9f8ec9fff577d40c2d1acbe8ecb
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-04-11 11:28:10 +0200

    Update ProjectsFragment.java
    navigation projects to add project

--- MyApplication/app/src/main/java/com/example/myapplication/ui/projects/ProjectsFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/projects/ProjectsFragment.java
@@ -6,14 +6,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.fragment.NavHostFragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentDashboardBinding;
 import com.example.myapplication.databinding.FragmentProjectsBinding;
+import com.example.myapplication.ui.dashboard.DashboardFragment;
+import com.example.myapplication.ui.timeRegistration.AllTimeRegistrationsFragment;
 
 
 public class ProjectsFragment extends Fragment {
@@ -25,6 +31,26 @@ private FragmentProjectsBinding binding;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProjectsBinding.inflate(inflater, container, false);
+        binding = FragmentProjectsBinding.inflate(inflater, container, false);
+
+
+        binding.buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //navigeer naar quick start fragment
+                NavHostFragment.findNavController(ProjectsFragment.this)
+                        .navigate(R.id.action_navigation_project_to_addProject);
+
+            }
+        });
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //navigeer naar quick start fragment
+                NavHostFragment.findNavController(ProjectsFragment.this)
+                        .navigate(R.id.action_navigation_project_to_detailsProject);
+            }
+        });
         View root = binding.getRoot();
         View view = inflater.inflate(R.layout.fragment_projects,container,false);
         list=(ListView) root.findViewById(R.id.listview);
