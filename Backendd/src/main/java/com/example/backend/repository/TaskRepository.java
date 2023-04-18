4f38cd4ffb57cfc04ed90625fd78d032374f271f
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-04-18 13:41:18 +0200

    Update TaskRepository.java
    edited

--- Backendd/src/main/java/com/example/backend/repository/TaskRepository.java
+++ Backendd/src/main/java/com/example/backend/repository/TaskRepository.java
@@ -1,9 +1,10 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends CrudRepository<Task, Integer> {
+public interface TaskRepository extends JpaRepository<Task, Integer> {
 }
