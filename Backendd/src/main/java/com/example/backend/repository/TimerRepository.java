b9b1afb9ebeac87ba46af605f4151090fddc26a0
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-04-18 13:40:57 +0200

    Update TimerRepository.java
    edited

--- Backendd/src/main/java/com/example/backend/repository/TimerRepository.java
+++ Backendd/src/main/java/com/example/backend/repository/TimerRepository.java
@@ -1,9 +1,10 @@
 package com.example.backend.repository;
 
 import com.example.backend.model.Timer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TimerRepository extends CrudRepository<Timer, Integer> {
+public interface TimerRepository extends JpaRepository<Timer, Integer> {
 }
