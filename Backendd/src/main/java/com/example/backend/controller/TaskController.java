a9b6c1747fc450b63710264f9929e198b27ece0a
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-04-24 14:14:40 +0200

    TaskController

---
+++ Backendd/src/main/java/com/example/backend/controller/TaskController.java
@@ -0,0 +1,58 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.Task;
+import com.example.backend.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+    @PostMapping("/create")
+    public ResponseEntity<Task> createTask(@RequestBody Task task) {
+        Task createdTask = taskService.createTask(task);
+        return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<Task>> getAllTasks() {
+        List<Task> tasks = taskService.getAllTasks();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<Task> getTaskById(@PathVariable("id") int taskId) {
+        Task task = taskService.getTaskById(taskId);
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Task> updateTask(@RequestBody Task task) {
+        taskService.updateTask(task);
+        return new ResponseEntity<>(task, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteTask(@PathVariable("id") int taskId) {
+        boolean success = taskService.deleteTask(taskId);
+        if (success) {
+            taskService.deleteTask(taskId);
+            return ResponseEntity.ok("Task met ID " + taskId + " succesvol verwijderd.");
+        } else {
+            return new ResponseEntity<>("Task met ID " + taskId + " bestaat niet", HttpStatus.NOT_FOUND);
+        }
+       // taskService.deleteTask(taskId);
+        //return ResponseEntity.ok("Task met ID " + taskId + " succesvol verwijderd.");
+    }
+
+}
+
+
