fd761174d115644ab06bb2a3b861f1dbba6bd23e
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-04-26 11:34:53 +0200

    ProjectService created

---
+++ MyApplication/app/src/main/java/com/example/myapplication/service/ProjectService.java
@@ -0,0 +1,23 @@
+package com.example.myapplication.service;
+
+import com.example.myapplication.data.model.Project;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface ProjectService {
+        @GET("projects/{id}")
+        Call<Project> getProjectById(@Path("id") int id);
+
+        @GET("projects")
+        Call<List<Project>> getAllProjects();
+
+
+    }
+
+
+
+
