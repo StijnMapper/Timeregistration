2edb18f8846447bd314f4c06151f2f39ef4257fa
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-02 13:11:47 +0200

    RetrofitClient Updated

--- MyApplication/app/src/main/java/com/example/myapplication/retrofit/RetrofitClient.java
+++ MyApplication/app/src/main/java/com/example/myapplication/retrofit/RetrofitClient.java
@@ -1,18 +1,5 @@
 package com.example.myapplication.retrofit;
 
-import static android.content.ContentValues.TAG;
-
-import android.util.Log;
-
-
-import com.example.myapplication.data.model.Project;
-import com.example.myapplication.service.ProjectService;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -27,33 +14,6 @@ public class RetrofitClient {
                         .baseUrl(BASE_URL)
                         .addConverterFactory(GsonConverterFactory.create())
                         .build();
-                // Create ProjectService instance
-                ProjectService service = retrofit.create(ProjectService.class);
-                // Call the getAllProjects() API endpoint asynchronously
-                Call<List<Project>> call = service.getAllProjects();
-                call.enqueue(new Callback<List<Project>>() {
-                    @Override
-                    public void onResponse(Call<List<Project>> call, Response<List<Project>> response) {
-                        if (response.isSuccessful()) {
-                            List<Project> projects = response.body();
-                            // Log successful response
-                            Log.d("Tag", "success");
-
-                        } else {
-                           // Log Error response
-                            Log.d("Tag", "Error");
-
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<List<Project>> call, Throwable t) {
-                        // Log failure response
-                        Log.d("Projects", "Faillure: " + t.getMessage());
-                    }
-                });
-
-
             }
             return retrofit;
         }
