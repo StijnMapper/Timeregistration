89e59e9e982b4d61165428563ffe6de4067f1a9f
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-04 10:45:56 +0200

    view edited + buttons add

--- MyApplication/app/src/main/java/com/example/myapplication/ui/DetailsProject/DetailsProjectViewHolder.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/DetailsProject/DetailsProjectViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.DetailsProject;
 
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +13,14 @@ public class DetailsProjectViewHolder extends RecyclerView.ViewHolder{
     TextView startTime;
     TextView endTime;
     TextView totalHours;
+    ImageButton delete;
+    ImageButton edit;
     public DetailsProjectViewHolder(@NonNull View itemView) {
         super(itemView);
         startTime=itemView.findViewById(R.id.startTime);
         endTime=itemView.findViewById(R.id.endTime);
         totalHours=itemView.findViewById(R.id.totalHours);
+        delete = itemView.findViewById(R.id.delete);
+        edit = itemView.findViewById(R.id.edit);
     }
 }
