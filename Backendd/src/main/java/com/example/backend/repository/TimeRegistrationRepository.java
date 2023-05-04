1aebe73925319a29c7cecbba66c8d48d9db5e814
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-04 13:08:32 +0200

    TimeRegistrationRepository updated

--- Backendd/src/main/java/com/example/backend/repository/TimeRegistrationRepository.java
+++ Backendd/src/main/java/com/example/backend/repository/TimeRegistrationRepository.java
@@ -2,10 +2,12 @@ package com.example.backend.repository;
 
 import com.example.backend.model.TimeRegistration;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TimeRegistrationRepository extends JpaRepository<TimeRegistration, Integer> {
+    List<TimeRegistration> findByProjectProjectId(int projectId);
 
 }
