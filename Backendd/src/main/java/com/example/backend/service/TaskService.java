d89b258338f3f025ff7429f6544baee22c9af0db
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-05 11:39:14 +0200

    TaskService created

--- Backendd/src/main/java/com/example/backend/service/TaskService.java
+++ Backendd/src/main/java/com/example/backend/service/TaskService.java
@@ -1,17 +1,43 @@
 package com.example.backend.service;
 
 import com.example.backend.model.Task;
+import com.example.backend.model.TimeRegistration;
 import com.example.backend.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Data
 @AllArgsConstructor
 public class TaskService {
-    private final TaskRepository taskRepository;
+    private TaskRepository taskRepository;
+    private TimeRegistrationService timeRegistrationService;
     public Task createTask(Task task) {
         return taskRepository.save(task);
     }
+
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTaskById(int taskId) {
+        return taskRepository.findById(taskId).orElseThrow(() ->  new NullPointerException("Task not found with id " + taskId));
+    }
+
+
+    public void updateTask(Task task) {
+        taskRepository.save(task);
+    }
+
+    public boolean deleteTask(int taskId) {
+        taskRepository.deleteById(taskId);
+        return false;
+    }
+
+
 }
