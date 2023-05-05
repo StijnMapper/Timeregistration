f60453db6361b261677e254bad2fb899e9ed4a61
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-05 11:42:31 +0200

    UserService created

--- Backendd/src/main/java/com/example/backend/service/UserService.java
+++ Backendd/src/main/java/com/example/backend/service/UserService.java
@@ -1,15 +1,35 @@
 package com.example.backend.service;
 
+import com.example.backend.model.User;
 import com.example.backend.repository.UserRepository;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Data
-@AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(int id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+
 
 
 }
