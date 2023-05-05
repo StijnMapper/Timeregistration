a451b5e82da25c29a309993d6e385e48dab5ce59
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-05 11:41:33 +0200

    controller method getTimeRegistrationsByProjectId updated with TimeRegistrationResponse

--- Backendd/src/main/java/com/example/backend/controller/TimeRegistrationController.java
+++ Backendd/src/main/java/com/example/backend/controller/TimeRegistrationController.java
@@ -1,13 +1,18 @@
 package com.example.backend.controller;
 
 import com.example.backend.model.TimeRegistration;
+import com.example.backend.response.TaskResponse;
+import com.example.backend.response.TimeRegistrationResponse;
+import com.example.backend.response.TimerResponse;
 import com.example.backend.service.TimeRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api")
@@ -16,12 +21,15 @@ public class TimeRegistrationController {
     private TimeRegistrationService timeRegistrationService;
 
     @GetMapping("/registrations/project/{projectId}")
-    public ResponseEntity<List<TimeRegistration>> getTimeRegistrationsByProjectId(@PathVariable int projectId) {
+    public ResponseEntity<List<TimeRegistrationResponse>> getTimeRegistrationsByProjectId(@PathVariable int projectId) {
         List<TimeRegistration> timeRegistrations = timeRegistrationService.getTimeRegistrationsByProjectId(projectId);
         if (timeRegistrations.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } else {
-            return new ResponseEntity<>(timeRegistrations, HttpStatus.OK);
+            List<TimeRegistrationResponse> responseList = timeRegistrations.stream()
+                    .map(TimeRegistrationResponse::new)
+                    .collect(Collectors.toList());
+            return new ResponseEntity<>(responseList, HttpStatus.OK);
         }
     }
 
@@ -37,7 +45,7 @@ public class TimeRegistrationController {
         List<TimeRegistration> timeRegistrations = timeRegistrationService.getAllTimeRegistrations();
         return new ResponseEntity<>(timeRegistrations, HttpStatus.OK);
     }
-    
+
     @GetMapping("get/{timeRegistrationId}")
     public ResponseEntity<TimeRegistration> getTimeRegistrationById(@PathVariable int timeRegistrationId) {
         TimeRegistration timeRegistration = timeRegistrationService.getTimeRegistrationById(timeRegistrationId);
@@ -48,7 +56,7 @@ public class TimeRegistrationController {
         }
     }
 
-    @PutMapping("update/{timeRegistrationId}")
+    @PutMapping("/update/{timeRegistrationId}")
     public ResponseEntity<TimeRegistration> updateTimeRegistration(@PathVariable int timeRegistrationId, @RequestBody TimeRegistration timeRegistrationDetails) {
         TimeRegistration updatedTimeRegistration = timeRegistrationService.updateTimeRegistration(timeRegistrationId, timeRegistrationDetails);
         if (updatedTimeRegistration == null) {
@@ -57,7 +65,6 @@ public class TimeRegistrationController {
             return new ResponseEntity<>(updatedTimeRegistration, HttpStatus.OK);
         }
     }
-
     @DeleteMapping("delete/{timeRegistrationId}")
     public ResponseEntity<String> deleteTimeRegistration(@PathVariable int timeRegistrationId) {
         boolean deleted = timeRegistrationService.deleteTimeRegistration(timeRegistrationId);
@@ -66,7 +73,7 @@ public class TimeRegistrationController {
         } else {
             return new ResponseEntity<>("Time registration with id " + timeRegistrationId + " not found.", HttpStatus.NOT_FOUND);
         }
-    }
-}
+    }}
+
 
 
