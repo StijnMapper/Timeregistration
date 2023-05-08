904e0f517c9c0ea7941b3a4e010f94af005a7c6f
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-08 16:28:30 +0200

    Update TimeRegistrationService
    Delete method edited

--- Backendd/src/main/java/com/example/backend/service/TimeRegistrationService.java
+++ Backendd/src/main/java/com/example/backend/service/TimeRegistrationService.java
@@ -3,11 +3,11 @@ package com.example.backend.service;
 import com.example.backend.model.TimeRegistration;
 import com.example.backend.repository.ProjectRepository;
 import com.example.backend.repository.TimeRegistrationRepository;
+import com.example.backend.repository.TimerRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.Optional;
 
 
 @Service
@@ -16,6 +16,7 @@ import java.util.Optional;
 public class TimeRegistrationService {
     private final TimeRegistrationRepository timeRegistrationRepository;
     private final ProjectRepository projectRepository;
+    private final TimerRepository timerRepository;
 
     public List<TimeRegistration> getTimeRegistrationsByProjectId(int projectId) {
         return timeRegistrationRepository.findByProjectProjectId(projectId);
@@ -23,32 +24,27 @@ public class TimeRegistrationService {
 
 
     public List<TimeRegistration> getAllTimeRegistrations() {
-            return timeRegistrationRepository.findAll();
-        }
+        return timeRegistrationRepository.findAll();
+    }
 
-        public TimeRegistration getTimeRegistrationById(int timeRegistrationId) {
-            return timeRegistrationRepository.findById(timeRegistrationId).orElse(null);
-        }
+    public TimeRegistration getTimeRegistrationById(int timeRegistrationId) {
+        return timeRegistrationRepository.findById(timeRegistrationId).orElse(null);
+    }
 
 
     public TimeRegistration createTimeRegistration(TimeRegistration timeRegistration) {
-            return timeRegistrationRepository.save(timeRegistration);
-        }
-    // TimeRegistrationService
-
+        return timeRegistrationRepository.save(timeRegistration);
+    }
 
     public TimeRegistration updateTimeRegistration(int timeRegistrationId, TimeRegistration timeRegistrationDetails) {
-            TimeRegistration timeRegistration = timeRegistrationRepository.findById(timeRegistrationId).orElse(null);
-
-                return timeRegistrationRepository.save(timeRegistration);
-        }
-
-
-
-    public boolean deleteTimeRegistration(int timeRegistrationId) {
         TimeRegistration timeRegistration = timeRegistrationRepository.findById(timeRegistrationId).orElse(null);
-        if (timeRegistration != null) {
-            timeRegistrationRepository.delete(timeRegistration);
+
+        return timeRegistrationRepository.save(timeRegistration);
+    }
+
+    public boolean deleteTimeRegistration(int registrationId) {
+        if (timeRegistrationRepository.existsById(registrationId)) {
+            timeRegistrationRepository.deleteById(registrationId);
             return true;
         } else {
             return false;
