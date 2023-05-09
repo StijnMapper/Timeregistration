a563d79691230a8baf351a214ad583707d708959
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-09 16:22:06 +0200

    navigate to details of timeregistration added

--- MyApplication/app/src/main/java/com/example/myapplication/ui/DetailsProject/DetailsProjectAdapter.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/DetailsProject/DetailsProjectAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.ui.DetailsProject;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
@@ -29,15 +31,16 @@ import retrofit2.Response;
 public class DetailsProjectAdapter extends RecyclerView.Adapter<DetailsProjectAdapter.RegistrationViewHolder> {
 
     private Context context;
-    private List<TimeRegistration> timeRegistrations= new ArrayList<>();
+    private List<TimeRegistration> timeRegistrations = new ArrayList<>();
 
     private DetailsProject fragment;
+
     public void setRegistrations(List<TimeRegistration> registrations) {
-        this.timeRegistrations=registrations;
+        this.timeRegistrations = registrations;
         notifyDataSetChanged();
     }
 
-  public DetailsProjectAdapter(Context context, List<TimeRegistration> timeRegistrations) {
+    public DetailsProjectAdapter(Context context, List<TimeRegistration> timeRegistrations) {
         this.context = context;
         this.timeRegistrations = timeRegistrations;
 
@@ -55,27 +58,41 @@ public class DetailsProjectAdapter extends RecyclerView.Adapter<DetailsProjectAd
     @Override
     public void onBindViewHolder(@NonNull RegistrationViewHolder holder, int position) {
         TimeRegistration timeRegistration = timeRegistrations.get(holder.getBindingAdapterPosition());
-            holder.startTime.setText(getFormattedTime(timeRegistration.getTimer().getStartTime()));
-            holder.endTime.setText(getFormattedTime(timeRegistration.getTimer().getEndTime()));
-
+        holder.startTime.setText(getFormattedTime(timeRegistration.getTimer().getStartTime()));
+        holder.endTime.setText(getFormattedTime(timeRegistration.getTimer().getEndTime()));
         holder.delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int timeRegistrationId = timeRegistrations.get(holder.getBindingAdapterPosition()).getRegistrationId();
-                int timerId = timeRegistrations.get(holder.getBindingAdapterPosition()).getTimer().getTimerId();
-                Log.d(DetailsProject.class.getSimpleName(), "registrationId " + timeRegistrationId + ", timerId " + timerId);
-                deleteTimeRegistration(timeRegistrationId);
+                int position = holder.getBindingAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    TimeRegistration timeRegistration = timeRegistrations.get(position);
+                    int timeRegistrationId = timeRegistration.getRegistrationId();
+                    int timerId = timeRegistration.getTimer().getTimerId();
+                    Log.d(DetailsProjectAdapter.class.getSimpleName(), "registrationId " + timeRegistrationId + ", timerId " + timerId);
+                    Log.d(DetailsProjectAdapter.class.getSimpleName(), String.valueOf(timeRegistration.getProject().getProjectId()));
+                    deleteTimeRegistration(timeRegistrationId, timeRegistration.getProject().getProjectId());
 
-            }});
+                }
+            }
 
+        });
+        //navigate to details of time registration
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                bundle.putInt("registrationId", timeRegistration.getRegistrationId());
+                Navigation.findNavController(view).navigate(R.id.action_detailsProject_to_withoutTimerFragment, bundle);
+            }
+        });
     }
-    public void deleteTimeRegistration(int timeRegistrationId) {
+
+    public void deleteTimeRegistration(int timeRegistrationId, int projectId) {
         TimeRegistrationService service = RetrofitClient.getRetrofitInstance().create(TimeRegistrationService.class);
-        Call<Void> callDelete = service.deleteTimeRegistration(timeRegistrationId);
+        Call<Void> callDelete = service.deleteTimeRegistrationsByProjectId(projectId,timeRegistrationId);
         callDelete.enqueue(new Callback<Void>() {
             @Override
             public void onResponse(Call<Void> call, Response<Void> response) {
-
                 if (response.isSuccessful()) {
                     // Remove the deleted time registration from the list
                     int indexToRemove = -1;
@@ -83,35 +100,33 @@ public class DetailsProjectAdapter extends RecyclerView.Adapter<DetailsProjectAd
                         TimeRegistration tr = timeRegistrations.get(i);
                         if (tr.getRegistrationId() == timeRegistrationId) {
                             indexToRemove = i;
-                            Log.d(DetailsProject.class.getSimpleName(), "registrationId " + timeRegistrationId);
+                            Log.d(DetailsProjectAdapter.class.getSimpleName(), "registrationId " + timeRegistrationId);
                             break;
                         }
                     }
-                    Log.d(DetailsProject.class.getSimpleName(), "registrationId " + timeRegistrationId);
 
                     if (indexToRemove >= 0) {
                         timeRegistrations.remove(indexToRemove);
                         notifyDataSetChanged();
-                        //(indexToRemove);
                     }
 
-                    Log.d(DetailsProject.class.getSimpleName(), "Time registration deleted successfully");
+                    Log.d(DetailsProjectAdapter.class.getSimpleName(), "Time registration deleted successfully");
                 } else {
-                    Log.e(DetailsProject.class.getSimpleName(), "Error deleting time registration: " + response.code());
+                    Log.e(DetailsProjectAdapter.class.getSimpleName(), "Error deleting time registration: " + response.code());
                 }
             }
 
             @Override
             public void onFailure(Call<Void> call, Throwable t) {
-                Log.e(DetailsProject.class.getSimpleName(), t.getMessage(), t);
+                Log.e(DetailsProjectAdapter.class.getSimpleName(), t.getMessage(), t);
             }
         });
 
-
     }
+
     @Override
     public int getItemCount() {
-        Log.d("RegistrationAdapter", "registrations: " +  timeRegistrations.size());
+        Log.d(DetailsProjectAdapter.class.getSimpleName(), "registrations: " +  timeRegistrations.size());
         if (timeRegistrations == null) {
             return 0;
         }
@@ -138,15 +153,10 @@ public class DetailsProjectAdapter extends RecyclerView.Adapter<DetailsProjectAd
             totalHours = itemView.findViewById(R.id.totalHours);
             delete = itemView.findViewById(R.id.delete);
             edit = itemView.findViewById(R.id.edit);
-
-
-
         }
 
     }
-    public TimeRegistration getItem(int position) {
-        return timeRegistrations.get(position);
-    }
+
     // method to format the time as a string of timeregistrations
     private String getFormattedTime(String timeString) {
         SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
