02cd0503d66c79897d1ec011d02976c6c72438b3
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-05-22 14:53:24 +0200

    get all timeregistrations

--- MyApplication/app/src/main/java/com/example/myapplication/ui/AllTimeRegistrations/AllTimeRegistrationsFragment.java
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/AllTimeRegistrations/AllTimeRegistrationsFragment.java
@@ -5,21 +5,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.activity.OnBackPressedCallback;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.myapplication.R;
 import com.example.myapplication.data.model.TimeRegistration;
 import com.example.myapplication.databinding.FragmentAllTimeRegistrationsBinding;
 import com.example.myapplication.retrofit.RetrofitClient;
 import com.example.myapplication.service.TimeRegistrationService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -27,14 +25,11 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AllTimeRegistrationsFragment extends Fragment {
-    private FragmentAllTimeRegistrationsBinding binding;
+
     private RecyclerView recyclerView;
-    List<TimeRegistration> registrations;
+    List<TimeRegistration> registrations =  new ArrayList<>();
 
     private MyAdapter myAdapter;
-    TextView item;
-
-    String[] data = {"time registration 1", "time registration 2", "time registration 3", "time registration4", "time registration 5", "time registration 6", "time registration 7", "time registration 8", "time registration 9", "time registration 10"};
 
     public AllTimeRegistrationsFragment() {
         // Required empty public constructor
@@ -43,13 +38,15 @@ public class AllTimeRegistrationsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+        /*
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default ) {
             @Override
             public void handleOnBackPressed() {
                 // Handle the back button event
             }
         };
         requireActivity().getOnBackPressedDispatcher().addCallback(this, callback);
+        */
 
     }
 
@@ -57,48 +54,40 @@ public class AllTimeRegistrationsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_all_time_registrations, container, false);
-        binding = FragmentAllTimeRegistrationsBinding.inflate(inflater, container, false);
-
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_all_time_registrations, container, false);
+        FragmentAllTimeRegistrationsBinding binding = FragmentAllTimeRegistrationsBinding.inflate(inflater, container, false);
 
         // Initialize RecyclerView and its adapter
-        RecyclerView recyclerView = binding.recyclerview;
         NavController navController = NavHostFragment.findNavController(this);
 
-        MyAdapter adapter = new MyAdapter(registrations, navController);
-        recyclerView.setAdapter(adapter);
+        myAdapter = new MyAdapter(getContext(), registrations);
+        recyclerView = binding.recyclerview;
+        recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-       TimeRegistrationService service = RetrofitClient.getRetrofitInstance().create(TimeRegistrationService.class);
-        //Retrieve all projects
-        Call<List<TimeRegistration>> call = service.getTimeRegistrations();
+        fetchData();
+        return binding.getRoot();
 
-        //asynchronous call to fetch all the projects and add the list of projects to the adapter.
-        //this done by using enqueue method of Call, which executes the call in a separate thread and
-        // processes the response in the callbacks that are defined.
+
+    }
+
+    private void fetchData() {
+        TimeRegistrationService service = RetrofitClient.getRetrofitInstance().create(TimeRegistrationService.class);
+        Call<List<TimeRegistration>> call = service.getAllTimeRegistrations();
+
         call.enqueue(new Callback<List<TimeRegistration>>() {
             @Override
             public void onResponse(Call<List<TimeRegistration>> call, Response<List<TimeRegistration>> response) {
-                //success? , the onResponse callback retrieves the list of projects from the response
-                // adds it to the adapter using the setProjects method.
                 if (response.isSuccessful()) {
                     registrations = response.body();
                     myAdapter.setRegistrations(registrations);
-                    Log.d("Tag", "Number of all registrations retrieved: " + registrations.size());
+                    Log.d(AllTimeRegistrationsFragment.class.getSimpleName(), "Number of all registrations retrieved: " + registrations.size());
                 }
             }
 
             @Override
             public void onFailure(Call<List<TimeRegistration>> call, Throwable t) {
-                Log.d("Tag", "Error: no list of registrations" + t.getMessage());
-
+                Log.d(AllTimeRegistrationsFragment.class.getSimpleName(), "Error: " + t.getMessage());
             }
         });
-
-        return binding.getRoot();
-
-
     }
+
 }
