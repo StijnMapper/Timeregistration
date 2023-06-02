7e686de8f0a55488b25971c3291677ab5fdc237e
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-06-02 09:23:49 +0200

    TimerService updated

--- Backendd/src/main/java/com/example/backend/service/TimerService.java
+++ Backendd/src/main/java/com/example/backend/service/TimerService.java
@@ -1,5 +1,6 @@
 package com.example.backend.service;
 
+import com.example.backend.model.TimeRegistration;
 import com.example.backend.model.Timer;
 import com.example.backend.repository.TimerRepository;
 import lombok.AllArgsConstructor;
@@ -14,29 +15,31 @@ import java.util.Optional;
 @AllArgsConstructor
 public class TimerService {
     private final TimerRepository timerRepository;
+    private TimeRegistrationService timeRegistrationService;
 
 
-        public Timer createTimer(Timer timer) {
-            return timerRepository.save(timer);
-        }
+    public Timer createTimer(Timer timer) {
+        return timerRepository.save(timer);
+    }
 
-        public Timer updateTimer(Timer timer) {
-            return timerRepository.save(timer);
-        }
+    public Timer updateTimer(Timer timer) {
+        return timerRepository.save(timer);
+    }
 
-        public void deleteTimer(int timerId) {
-            timerRepository.deleteById(timerId);
-        }
+    public boolean deleteTimer(int timerId) {
+        timerRepository.deleteById(timerId);
+        return false;
+    }
 
-        public List<Timer> getAllTimers() {
-            return timerRepository.findAll();
-        }
 
-        public Timer getTimerById(int timerId) {
-            Optional<Timer> timer = timerRepository.findById(timerId);
-            return timer.orElseThrow(() -> new NullPointerException("Timer not found"));
-        }
+    public List<Timer> getAllTimers() {
+        return timerRepository.findAll();
+    }
 
+    public Timer getTimerById(int timerId) {
+        Optional<Timer> timer = timerRepository.findById(timerId);
+        return timer.orElseThrow(() -> new NullPointerException("Timer not found"));
+    }
 
 
 }
