fa2a9744b8e79e265b414324fa913a762f7532df
TevhideCamur <tevhide.camur@inetum-realdolmen.world>
2023-06-02 09:26:51 +0200

    Usercontroller

---
+++ Backendd/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.User;
+import com.example.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+
+}
